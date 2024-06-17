672fb7b4f7fadff230a0e02a555e8d97f3932545
Bob Weigel <rweigel@gmu.edu>
2024-06-16 20:25:25 -0400

    startup

--- table/etc/cdawmeta-dynu.sh
+++ table/etc/cdawmeta-dynu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source /Users/weigel/miniconda3/etc/profile.d/conda.sh; conda activate
+source /Users/weigel/opt/anaconda3/etc/profile.d/conda.sh; conda activate
 conda activate base
 
-cd ../; make serve-cdaweb-sql
+cd ../
+make serve-cdaweb-sql &
+make serve-hapi-sql
