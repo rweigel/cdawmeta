a917443659e8c57390256bdfa4709f2b215ad534
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-09-24 16:29:50 -0400

    typo

--- table.py
+++ table.py
@@ -4,7 +4,7 @@ import cdawmeta
 args = cdawmeta.cli('table.py')
 
 if args['port'] is not None and args['table_name'] is None:
-  raise ValueError("Must specify --table_name when --port is specified")
+  raise ValueError("Must specify --table-name when --port is specified")
 
 port = args['port']
 del args['port']
