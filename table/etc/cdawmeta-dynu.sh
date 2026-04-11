811fc4f861a9545178234e50a416cc2b406cdb63
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-04-11 08:19:32 -0400

    tableui startup script

--- table/etc/cdawmeta-dynu.sh
+++ table/etc/cdawmeta-dynu.sh
@@ -4,5 +4,4 @@ source ~/anaconda3/etc/profile.d/conda.sh; conda activate
 conda activate python3.10.9-cdawmeta
 
 pkill -f "python serve.py --port 8051"
-cd ../../../table-ui;
-python serve.py --port 8051 --config ../cdawmeta/table/conf/cdaweb.json
+tableui-serve --port 8051 --config ../conf/cdaweb.json
